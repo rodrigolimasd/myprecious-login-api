cff75794f702fb513d91230335d47d574fbb641c
Rodrigo Lima <rodrigolimasd@gmail.com>
2020-12-08 18:39:52 -0300

    primeiro commit

---
+++ src/main/java/com/rodtech/mypreciousloginapi/MypreciousLoginApiApplication.java
@@ -0,0 +1,13 @@
+package com.rodtech.mypreciousloginapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypreciousLoginApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MypreciousLoginApiApplication.class, args);
+	}
+
+}
